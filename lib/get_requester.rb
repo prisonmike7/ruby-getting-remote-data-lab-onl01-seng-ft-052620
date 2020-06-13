ebc5266302432ebf48599600ac375fe8abdacf35
Michael Harris <mwharris7@gmail.com>
2020-06-13 23:58:26 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+# require 'awesome_print'
+
+class GetRequester
+
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        JSON.parse(response.body)
+    end
+
+end
