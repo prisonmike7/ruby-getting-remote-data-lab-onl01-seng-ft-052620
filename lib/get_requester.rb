65df57cd78177aae39c328f115a9337b9e9e811c
Michael Harris <mwharris7@gmail.com>
2020-06-13 23:58:10 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -10,13 +10,13 @@ class GetRequester
     def initialize(url)
         @url = url
     end
-    
+
     def get_response_body
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         response.body
     end
-    
+
     def parse_json
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
